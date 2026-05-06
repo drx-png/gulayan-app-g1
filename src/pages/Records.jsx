e48ef2ca33636125124a831569d4bbfd1889e872
Ron <wondelrosario@gmail.com>
2026-05-06 09:01:17 +0800

    update demo

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -23,14 +23,17 @@ function Records() {
 
   const handleSearchPlants = async () => {
     // TODO search from the the backend; in case that all records is not yet loaded
+    // update 3
   }
   const handleLoadRecords = async (page = 1, append = false) => {
     //TODO: load the data from the database
     //TODO: implement paginated data loading
+    // update 2
   }
   const handleAddRecord = async (formData) => {
     try {
       //TODO: make add new record functional
+      //update 1
       toast.success("New record saved.");
     } catch (error) {
       console.error(error);
